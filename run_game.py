fe32fc7dada226f220f3d216cf071d89ad41f705
Alexander Malmberg <alexander@malmberg.org>
2014-05-15 14:23:38 +0100

    Update test code to try out the map rendering code.

--- run_game.py
+++ run_game.py
@@ -9,12 +9,21 @@ from OpenGL import GL
 
 import render_state
 import text
+import world
+import world_render
 
 
 def main():
   pygame.init()
-  flags = pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE
-  width, height = 1024, 640
+
+  fullscreen = False
+  if fullscreen:
+    flags = (pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE
+             | pygame.FULLSCREEN)
+    width, height = 0, 0
+  else:
+    flags = pygame.OPENGL | pygame.DOUBLEBUF | pygame.HWSURFACE
+    width, height = 1024, 640
 
   pygame.display.set_caption('AI wars 2000 Deluxe Edition!')
   screen = pygame.display.set_mode((width, height), flags)
@@ -23,6 +32,31 @@ def main():
 
   clock = pygame.time.Clock()
 
+  if 1:
+    w = world.World(16, 16)
+    w.map[5][5] = 1
+    w.map[6][5] = 1
+    w.map[7][5] = 1
+    w.map[8][5] = 1
+    w.map[6][6] = 1
+    w.map[7][6] = 1
+    w.map[8][6] = 1
+    w.map[7][7] = 1
+  else:
+    w = world.World(3, 3)
+    w.map[0][0] = 1
+    w.map[1][1] = 1
+    w.map[2][2] = 1
+
+    # LLL WWW WWW
+    # LLL WWW WWW
+    # LLL WWW WWW
+    # WWW WWW WWW
+    # WWW WWW WWW
+    # WWW WWW WWW
+
+  wr = world_render.WorldRenderer(render, w)
+
   t = text.Text(render)
   GL.glEnable(GL.GL_BLEND)
 
@@ -30,6 +64,7 @@ def main():
     clock.tick()
     GL.glClear(GL.GL_COLOR_BUFFER_BIT)
     t.DrawString(-1.6, -1.0, 0.05, (1, 1, 1, 1), str(clock))
+    wr.Draw()
     pygame.display.flip()
 
 
