0e5a56f91274108a54eb7776cae47ba943d1eeeb
Alexander Malmberg <alexander@malmberg.org>
2014-05-17 23:56:28 +0100

    Add random names.

--- g_app.py
+++ g_app.py
@@ -1,5 +1,21 @@
+import random
+
 import g_base
 
 
+NamePart1 = ['Angry', 'Upset', 'Funny', 'Bloppy']
+NamePart2 = ['Rhinos', 'Hippos', 'Giraffes', 'Zebras']
+
+def RandomAppName():
+  return (NamePart1[random.randint(0, len(NamePart1) - 1)]
+          + ' '
+          + NamePart2[random.randint(0, len(NamePart2) - 1)])
+
+
 class App(g_base.Global):
-  pass
+  name = None
+
+  def Description(self):
+    if self.name is None:
+      self.name = RandomAppName()
+    return self.name
--- g_botnet.py
+++ g_botnet.py
@@ -1,5 +1,18 @@
+import random
+
 import g_base
 
 
+NameParts = ['Quux', 'Foo', 'Zot']
+
+def RandomBotnetName():
+  return NameParts[random.randint(0, len(NameParts) - 1)] + 'Net'
+
+
 class Botnet(g_base.Global):
-  pass
+  name = None
+
+  def Description(self):
+    if self.name is None:
+      self.name = RandomBotnetName()
+    return self.name
