28003f352b1a3fc9754517ac1677d94ce5f485ff
Alexander Malmberg <alexander@malmberg.org>
2014-05-18 23:21:49 +0100

    Update stealable. Factories build killer robots.

--- n_factory.py
+++ n_factory.py
@@ -1,6 +1,8 @@
 import icons
 import misc
 import n_stealable
+import n_unit
+import vec
 
 
 class Factory(n_stealable.Stealable):
@@ -9,32 +11,37 @@ class Factory(n_stealable.Stealable):
   def __init__(self, pos, owner, crack_defense, build_per_turn):
     super(Factory, self).__init__(pos, owner, crack_defense)
     self.build_per_turn = build_per_turn
-    self.building = False
-    self.target = None
     self.progress = 0
-    self.cost = 0
-
-  def StartBuilding(self, game_state, target):
-    self.cost = self.target.BuildCost()
-    if not game_state.UseRawMaterials(self.cost):
-      return
-    self.building = True
-    self.target = target
-
-  def StopBuilding(self):
-    self.building = False
-
-  def Discoverable(self):
-    return self.building
+    self.cost = 50
 
   def EndOfTurnUpdate(self, game_state):
-    if self.control and self.building:
-      self.progress += self.steal_fraction * self.build_per_turn
+    if self.control:
+      self.progress += self.build_per_turn
       if self.progress > self.cost:
-        self.target.BuiltOne(self, game_state)
-        self.building = False
+        self.BuildOne(game_state)
+        self.progress = 0
 
     super(Factory, self).EndOfTurnUpdate(game_state)
+
+  def BuildOne(self, game_state):
+    for dy in xrange(-1, 2):
+      for dx in xrange(-1, 2):
+        if not dx and not dy:
+          continue
+        tx = self.pos.x + dx
+        ty = self.pos.y + dy
+        if not game_state.Empty(tx, ty):
+          continue
+        if not game_state.world.LandAt(tx, ty):
+          continue
+        self.PlaceUnitAt(game_state, tx, ty)
+        return
+
+  def PlaceUnitAt(self, game_state, tx, ty):
+    unit = n_unit.Unit(
+      vec.Vec(tx, ty), game_state.ai_owner, False,
+      icons.KillerRobot, 10, 40, 4, True)
+    game_state.AddNode(unit)
 
   def __attrrepr__(self):
     return (super(Factory, self).__attrrepr__()
